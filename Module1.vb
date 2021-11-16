7cc67f3c9ef3030b07b76edcf84d899cb5f79350
AamnaSaleem <93542393+AamnaSaleem@users.noreply.github.com>
2021-11-16 21:13:44 +0500

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim Str1, Str2 As String
+
+        Console.Write("Enter sting to be processed: ")
+        Str1 = Console.ReadLine
+
+        If Len(Str1) < 3 Then
+            Console.WriteLine(Str1)
+            Console.ReadKey()
+            End
+        End If
+
+        If Right(Str1, 3) = "ing" Then
+            Str2 = Str1 & "ly"
+        Else
+            Str2 = Str1 & "ing"
+        End If
+
+        Console.WriteLine("Processed string is: " & Str2)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
